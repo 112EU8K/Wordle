61ee6d1d3c346ab0018090c6db397109d991b080
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-07 03:23:50 +0300

    Update KeyboardWnd.h

--- src/KeyboardWnd.h
+++ src/KeyboardWnd.h
@@ -4,7 +4,10 @@
 #include "WndInterface.h"
 #include "Button.h"
 
-
+/*
+Keyboard class:
+Defines a keyboard that positions itself at the bottom centrally within the specified bounds.
+*/
 class KeyboardWnd :
 	public WndInterface
 {
