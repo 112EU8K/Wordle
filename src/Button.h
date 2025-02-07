367203d1bb97fac1bc21ecde9f85ac278b4df6c9
112EU8K <90638908+112EU8K@users.noreply.github.com>
2025-02-07 03:35:08 +0300

    Update Button.h

--- src/Button.h
+++ src/Button.h
@@ -4,7 +4,12 @@
 
 #include <SFML/Graphics.hpp>
 
-
+/**
+ * Button class:
+ * Defines a simple button consisting of a rectangle region,
+ * some text to centre in it, a hover state, and an
+ * actionID that is available to give the button context.
+ */
 class Button
 {
 public:
